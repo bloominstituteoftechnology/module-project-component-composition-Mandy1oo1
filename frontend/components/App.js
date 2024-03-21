b3441742fd9ea2d482c60441ddea7def59d8e7fb
Mandy1oo1 <155116986+Mandy1oo1@users.noreply.github.com>
2024-03-21 13:22:31 -0700

    Add files via upload

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,46 +5,33 @@ import Card from './Card'
 const api_key = 'DEMO_KEY'
 const URL = `https://api.nasa.gov/planetary/apod?api_key=${api_key}`
 
-function App() {
-  const [apod, setApod] = useState()
+export default function App() {
+  const [data, setData] = useState()
 
   useEffect(() => {
-    function fetchPhoto() {
+    function fetchAPOD() {
       axios.get(URL)
       .then(res => {
-        console.log(res.data)
-        setApod(res.data)
+        setData(res.data)
       })
       .catch(err => {
         console.log(err.message)
       })
     }
-    fetchPhoto()
-   setApod(
-      {
-          "date": "2024-03-18",
-          "explanation": "A bright comet will be visible during next month's total solar eclipse.  This very unusual coincidence occurs because Comet 12P/Pons-Brooks's return to the inner Solar System places it by chance only 25 degrees away from the Sun during Earth's April 8 total solar eclipse.  Currently the comet is just on the edge of visibility to the unaided eye, best visible with binoculars in the early evening sky toward the constellation of the Fish (Pisces). Comet Pons-Brooks, though, is putting on quite a sh...",
-          "hdurl": "https://apod.nasa.gov/apod/image/2403/CometPonsBrook_Vallestad_2564.jpg",
-          "media_type": "image",
-          "service_version": "v1",
-          "title": "Comet Pons-Brooks' Swirling Coma",
-          "url": "https://apod.nasa.gov/apod/image/2403/CometPonsBrook_Vallestad_960.jpg"
-        },
-  )
+    fetchAPOD()
   }, [])
 
-  if (!apod) return 'Fetching Photo of the Day...'
+  if (!data) return <p>Fetching data...</p>
   return (
     <section>
       <Card
-      title={apod.title}
-      text={apod.explanation}
-      imageUrl={apod.url}
-      date={apod.date}
+      title={data.title}
+      text={data.explanation}
+      image={data.url}
+      author={data.service_version}
+      date={data.date}
       />
      
     </section>
   )
 }
-
-export default App
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default function Figure({imageUrl, caption}) {
+const StyledFigure = styled.figure`
+max-width: 60%;
+
+figcaption {
+  font-style: italic;
+}
+`
+
+export default function Figure({image, author, date}) {
     return (
-      <figure>
-        <img src={imageUrl} />
-        <figcaption>Awesome pic taken on {caption}</figcaption>
-      </figure>
+      <StyledFigure>
+        <img src={image} />
+        <figcaption>
+          The Photo of the Day by {author} on {date}
+          </figcaption>
+      </StyledFigure>
     )
     }
